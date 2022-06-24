bae317bc3a1d494617a3f9de476cdda094006c5e
ammar29.11 <ammarabozene@gmail.com>
2022-06-24 17:59:33 +0300

    project upgraded222

--- app/src/main/java/com/example/loginsignupapp/AdapterCar.java
+++ app/src/main/java/com/example/loginsignupapp/AdapterCar.java
@@ -1,14 +1,23 @@
 package com.example.loginsignupapp;
 
+import static android.content.ContentValues.TAG;
+
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.squareup.picasso.Picasso;
 
 import java.io.Serializable;
 import java.util.List;
@@ -19,31 +28,26 @@ public class AdapterCar extends RecyclerView.Adapter<AdapterCar.ViewHolder> {
     private List<Car> mData;
     private LayoutInflater mInflater;
     private Context context;
-
+    private FirebaseServices fbs;
 
     private final AdapterCar.ItemClickListener mClickListener = new ItemClickListener() {
         @Override
         public void onItemClick(View view, int position) {
-            // get restaurant data
             Car car = mData.get(position);
-            // upload restaurant data
-            // goto details activity
             Intent i = new Intent(context, CarDetailsActivity.class);
-            i.putExtra("car", (Serializable)car);
+            i.putExtra("car", car);
+
             context.startActivity(i);
         }
     };
-
 
     // data is passed into the constructor
     AdapterCar(Context context, List<Car> data) {
         this.mInflater = LayoutInflater.from(context);
         this.mData = data;
         this.context = context;
-
+        this.fbs = FirebaseServices.getInstance();
     }
-
-
 
     // inflates the row layout from xml when needed
     @Override
@@ -55,9 +59,20 @@ public class AdapterCar extends RecyclerView.Adapter<AdapterCar.ViewHolder> {
     // binds the data to the TextView in each row
     @Override
     public void onBindViewHolder(AdapterCar.ViewHolder holder, int position) {
-       Car car = mData.get(position);
+        Car car = mData.get(position);
         holder.tvName.setText(car.getName());
-        //holder.ivPhoto.setImageDrawable(rest.getPhoto());
+        fbs.getStorage().getReference().child(car.getPhoto()).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(car.getPhoto()).into(holder.ivPhoto);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.e(TAG, e.getMessage());
+            }
+        });
+
     }
 
     // total number of rows
@@ -66,8 +81,7 @@ public class AdapterCar extends RecyclerView.Adapter<AdapterCar.ViewHolder> {
         return mData.size();
     }
 
-
-    // stores and recycles views as they are scrolled off screen
+    // stores and recycles views as they are scrol    led off screen
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tvName;
         ImageView ivPhoto;
@@ -82,11 +96,6 @@ public class AdapterCar extends RecyclerView.Adapter<AdapterCar.ViewHolder> {
         @Override
         public void onClick(View view) {
             if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
-
-            //Open new page: Intent i = new Intent(, SignupActivity.class);
-
-
-
         }
     }
 
@@ -94,12 +103,6 @@ public class AdapterCar extends RecyclerView.Adapter<AdapterCar.ViewHolder> {
     Car getItem(int id) {
         return mData.get(id);
     }
-
-    // allows clicks events to be caught
-    /*
-    void setClickListener(AdapterRestaurant.ItemClickListener itemClickListener) {
-        this.mClickListener = itemClickListener;
-    }*/
 
     // parent activity will implement this method to respond to click events
     public interface ItemClickListener {
